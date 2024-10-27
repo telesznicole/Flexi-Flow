629ec5a1603a5ca74d8ccb02ba13d09eb66f5bb6
Oniwan <lzaozao91@gmail.com>
2024-10-27 12:19:26 +0800

    压缩图片

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import viteImagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/telesznicole.github.io/',
-  plugins: [react()],
+  plugins: [react(), viteImagemin()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
