deee4e24eb31e6c30378550dbccff4a6b494c5ea
Nicole Telesz <telesznicole@ufl.edu>
2024-11-09 13:34:59 -0500

    created CRUD operations
    created new branch off develop to work on CRUD; created CRUD folder to store all files with CRUD operations; put all profile pictures into ‘pictures’ folder; created functioning CRUD operations for users

---
+++ CRUD/users/navigation.php
@@ -0,0 +1,35 @@
+<style>
+body {
+	padding-left: 20px;
+}
+</style>
+
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!-- CHANGE THIS - Temporary CDN for FontAwesome just to get it up and running-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.0/css/all.css" rel="stylesheet">  
+
+
+<?php
+
+ini_set('display_errors','on');
+
+
+echo "<h2><strong>Flexi-Flow Database</h2></strong> || "; 
+echo "<a href='createUser.php'>Create User || </a>";
+echo "<a href='selectUserToModify.php'>Update User || </a>";
+
+
+
+echo "<br />";
+echo "<hr />";
+
+?>
