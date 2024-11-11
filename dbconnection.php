e8a2d0967e88e1c411bd2b5fc736058c2e60b504
Nicole Telesz <telesznicole@ufl.edu>
2024-11-10 19:48:17 -0500

    updated to be online

--- dbconnection.php
+++ dbconnection.php
@@ -4,19 +4,17 @@
 
 //Local PC
 
-$servername = "localhost:8889"; //"localhost: 3306"
-$username = "root";
-$password = "root";
-$dbname = "Flexi-Flow";
+// $servername = "localhost:8889"; //"localhost: 3306"
+// $username = "root";
+// $password = "root";
+// $dbname = "Flexi-Flow";
 
 
 //Online
-/*
 $servername = "localhost";
-$username = "your-db-user-here";
-$password = "your-db-pass-here";
-$dbname = "your-mysql-db-name-here";
-*/
+$username = "flexiflo_mydbuser8080";
+$password = "8#*XKi(Ldd6s";
+$dbname = "flexiflo_Flexi-Flow";
 
 
 ?>
